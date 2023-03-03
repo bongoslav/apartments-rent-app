bde30d33f23abb58d1fa8592171a40a1cb3e36f4
bongoslav <beborisov99@gmail.com>
2023-03-04 01:59:05 +0200

    deleted unnecessary import

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 const authController = require("../controllers/auth");
-const authMiddleware = require('../middleware/auth')
 
 router.get("/login", authController.getLogin);
 router.get("/signup", authController.getSignup);
